4fbe005c49eba01741cb6bc2025a84a3ac932c16
TTLES33 <jangrygar5@gmail.com>
2020-11-02 15:59:26 +0100

    Glitch Update

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => res.send('Hello World!'));
-
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
-
-
-
-
 
 
 
@@ -176,4 +165,4 @@ message.channel.send(warnStatus);
 })
 
 
-client.login(process.env.DISCORD_TOKEN);
+client.login(process.env.TOKEN);
